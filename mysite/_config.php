7f671de30c002225c61b40f25a512c492673ed47
Stig Lindqvist <stig@silverstripe.com>
2012-06-29 13:53:32 +1200

    Importing SS Framework 3.0

---
+++ mysite/_config.php
@@ -0,0 +1,21 @@
+<?php
+
+global $project;
+$project = 'mysite';
+
+global $database;
+$database = 'SS_mysite';
+
+require_once('conf/ConfigureFromEnv.php');
+
+MySQLDatabase::set_connection_charset('utf8');
+
+// Set the current theme. More themes can be downloaded from
+// http://www.silverstripe.org/themes/
+SSViewer::set_theme('simple');
+
+// Set the site locale
+i18n::set_locale('en_US');
+
+// Enable nested URLs for this site (e.g. page/sub-page/)
+if (class_exists('SiteTree')) SiteTree::enable_nested_urls();
