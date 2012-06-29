aae3521a61a1da94bcc2835871a24a1be09713a3
Stig Lindqvist <stig@silverstripe.com>
2012-06-29 15:07:25 +1200

    Allow env to override database settings and config facebook login

--- mysite/_config.php
+++ mysite/_config.php
@@ -4,7 +4,7 @@ global $project;
 $project = 'mysite';
 
 global $database;
-$database = 'SS_mysite';
+$database = 'SS_chasey';
 
 require_once('conf/ConfigureFromEnv.php');
 
@@ -19,3 +19,18 @@ i18n::set_locale('en_US');
 
 // Enable nested URLs for this site (e.g. page/sub-page/)
 if (class_exists('SiteTree')) SiteTree::enable_nested_urls();
+
+FacebookConnect::set_app_id(getenv('FACEBOOK_APP_ID'));
+FacebookConnect::set_api_secret(getenv('FACEBOOK_APP_SECRET'));
+FacebookConnect::set_lang('en_US');
+
+global $databaseConfig;
+// Support for Environment configuration
+if(getenv('MYSQL_DB_HOST')) {
+	$databaseConfig['type'] = 'MySQLDatabase';
+	$databaseConfig['server'] = getenv('MYSQL_DB_HOST');
+	$databaseConfig['username'] = getenv('MYSQL_USERNAME');
+	$databaseConfig['password'] = getenv('MYSQL_PASSWORD');
+	$databaseConfig['database'] = getenv('MYSQL_DB_NAME');
+	
+}
